aae76a79309345b5d7284991a5075aa63f80c05e
khoavadienq <ddkhoa9@gmail.com>
2025-11-15 15:08:34 +0700

    setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
